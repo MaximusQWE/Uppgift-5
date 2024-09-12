ddfd20cae3ba5dbf2e82b2ccefb6b68526d40ea9
mani06001 <mani06001@E-PW05C37P>
2024-09-12 12:44:59 +0200

    Add project files.

---
+++ Uppgift 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Uppgift_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Your pc ran into a problem and needs a restard")
+        }
+    }
+}
